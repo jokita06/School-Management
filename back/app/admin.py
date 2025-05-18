73fd3eab20cae5b0539f2e6b776433ed7a2ccc7b
Joyce Kelly <143757169+jokita06@users.noreply.github.com>
2025-05-18 11:47:10 -0300

    Configurando painel administrativo para gerenciamento de funcionários, disciplinas e ambientes de aula.

--- back/app/admin.py
+++ back/app/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Funcionario, Disciplina, AmbienteAula
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+class FuncionarioAdmin(UserAdmin):
+
+    # Campos para edição do admin
+    fieldsets = UserAdmin.fieldsets + (
+        ('Informações Adicionais', {'fields': ('NI', 'dt_nascimento', 'telefone', 'cargo',)}),
+    )
+    # Campos para criação 
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('NI', 'first_name', 'last_name', 'email', 'telefone', 'dt_nascimento', 'cargo',)}),
+    )
+
+
+# Registro dos modelos
+admin.site.register(Funcionario, FuncionarioAdmin)  
+admin.site.register(Disciplina)  
+admin.site.register(AmbienteAula)  
